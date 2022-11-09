c38fc2872c2d9edc6930f6914f536dd2149345d8
Madmozak <razqriz@gmail.com>
2022-11-09 15:19:30 +0100

    GUI changed option

--- Assets/Turret Weapon System/Resources/TurretElements/TurretConstruction.cs
+++ Assets/Turret Weapon System/Resources/TurretElements/TurretConstruction.cs
@@ -7,16 +7,26 @@ using UnityEngine;
 public class TurretConstruction : Editor
 {
     private GameObject[] weapons;
+    private GameObject[] turretBases;
+    private GameObject[] turretMainComponents;
     private string[] weaponList1;
     private string[] weaponList2;
+    private string[] turretBasesList;
+    private string[] turretMainComponentsList;
     int indexWeaponList1 = 0;
     int indexWeaponList2 = 0;
+    int turretBasesIndex = 0;
+    int turretMainComponentsIndex = 0;
 
     void OnEnable()
     {
         weapons = Resources.LoadAll<GameObject>("Weapons");
+        turretBases = Resources.LoadAll<GameObject>("TurretElements/TurretBases");
+        turretMainComponents = Resources.LoadAll<GameObject>("TuretMains");
         weaponList1 = new string[weapons.Length];
         weaponList2 = new string[weapons.Length];
+        turretBasesList = new string[turretBases.Length];
+        turretMainComponentsList = new string[turretMainComponents.Length];
         
         foreach (var item in weapons)
         {
@@ -25,25 +35,55 @@ public class TurretConstruction : Editor
             indexWeaponList1++;
             indexWeaponList2++;
         }
-        indexWeaponList1 = 0;
-        indexWeaponList2 = 0;
+
+        foreach (var item in turretBases)
+        {
+            turretBasesList[turretBasesIndex] = item.ToString();
+            turretBasesIndex++;
+        }
+
+        foreach (var item in turretMainComponents)
+        {
+            turretMainComponentsList[turretMainComponentsIndex] = item.ToString();
+            turretMainComponentsIndex++;
+        }
+
+        
     }
 
     public override void OnInspectorGUI()
     {
         
         DrawDefaultInspector();
-
+        
         GUILayout.Space(20);
         GUILayout.Label("CONSTRUCTION SCRIPT", EditorStyles.boldLabel);
         GUILayout.Space(10);
-        GUILayout.Label("Weapon1 List");
+        GUILayout.Label("Turret Base");
+        turretBasesIndex = EditorGUILayout.Popup(turretBasesIndex, turretBasesList);
+        GUILayout.Label("Turet Main Component");
+        turretMainComponentsIndex = EditorGUILayout.Popup(turretMainComponentsIndex, turretMainComponentsList);
+        GUILayout.Label("Weapon1");
         indexWeaponList1 = EditorGUILayout.Popup(indexWeaponList1, weaponList1);
-        GUILayout.Label("Weapon2 List");
+        GUILayout.Label("Weapon2");
         indexWeaponList2 = EditorGUILayout.Popup(indexWeaponList2, weaponList2);
         var turretBaseScript = target as TurretBase;
 
-        
+        if (GUI.changed)
+        {
+            turretBaseScript.weapons.Clear();
+            foreach (var item in turretBaseScript.weaponAttachements)
+            {
+                for (var i = item.childCount - 1; i >= 0; i--)
+                {
+                    DestroyImmediate(item.GetChild(i).gameObject);
+                }
+
+                turretBaseScript.AttachWeapons();
+            }
+            turretBaseScript.weapons.Add(Instantiate(weapons[indexWeaponList1], turretBaseScript.weaponAttachements[0]));
+        }
+
         GUILayout.Label("Weapons");
         if(GUILayout.Button("Build Weapons", GUILayout.Width(100)))
         {
@@ -52,14 +92,18 @@ public class TurretConstruction : Editor
             {
                 for (var i = item.childCount - 1; i >= 0; i--)
                 {
-                    Object.DestroyImmediate(item.GetChild(i).gameObject);
+                    DestroyImmediate(item.GetChild(i).gameObject);
                 }
 
                 turretBaseScript.AttachWeapons();
             }
 
-            turretBaseScript.weapons.Add(Instantiate(weapons[indexWeaponList1], turretBaseScript.weaponAttachements[0]));
-            turretBaseScript.weapons.Add(Instantiate(weapons[indexWeaponList2], turretBaseScript.weaponAttachements[1]));
+            if(!(indexWeaponList1 > weaponList1.Length) || !(indexWeaponList2 > weaponList2.Length))
+            {
+                turretBaseScript.weapons.Add(Instantiate(weapons[indexWeaponList1], turretBaseScript.weaponAttachements[0]));
+                turretBaseScript.weapons.Add(Instantiate(weapons[indexWeaponList2], turretBaseScript.weaponAttachements[1]));
+            }
+            
 
         }
 
